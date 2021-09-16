8495f8f4533b691f57021c1608410d0b9fd6b50f
Chris <Chris@margaritas-air.attlocal.net>
2021-09-15 20:27:24 -0700

    wednesday night commit

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,39 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react'
 import VideoPlayer from "./videoplayer"
 import { graphql, StaticQuery } from 'gatsby'
 
+import './popup.css'
+
 
 const Popup = (props) => {
+console.log('props', props)
+const [links, setLinks] = useState(null);
 
+const newFunc = () => {
+    {props.url.map(y => (
+        y.node.video.map(x => (
+            setLinks(x.id)
+        ))
+    ))}
+}
+
+console.log('id', links)
 
 return (props.trigger) ? (
     <div className="popup">
         <div className="popup-inner">
             <p>This is the popup component</p>
-        <StaticQuery
+
+            <div>
+            <VideoPlayer 
+                    videoSrcURL={null}
+                    // videoTitle={data.allContentfulVideoPost.video[0].title}
+                />
+            </div>
+
+
+        {/* <StaticQuery
             query={graphql`
-                query  {
-                    contentfulVideoPost {
-                        video {
-                            file {
+                query {
+                    allContentfulVideoPost {
+                        edges {
+                          node {
+                            video {
+                              file {
                                 url
-                             }
+                              }
+                              id
+                              title
+                            }
+                          }
                         }
+                      }
                     }
-                }
             `}
             render={data => (
                 <VideoPlayer 
-                    videoSrcURL={data.contentfulVideoPost.video[0].file.url}
-                    // videoTitle={data.contentfulVideoPost.video[0].title}
+                    videoSrcURL={data.node.video[0].file.url}
+                    // videoTitle={data.allContentfulVideoPost.video[0].title}
                 />
             )}
-            />
+            /> */}
         <button className="close-btn" onClick={() => props.setTrigger(false)}>close</button>
         </div>
     </div>
-) : "";
+) : null;
 }
 
 export default Popup;
 
+export const popupQuery = graphql`
+    query {
+      allContentfulVideoPost {
+        edges {
+          node {
+            slug
+            video {
+              id
+              description
+              title
+              file {
+                url
+              }
+            }
+            title
+            image {
+              file {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+`
--- src/pages/videos.js
+++ src/pages/videos.js
@@ -1,24 +1,53 @@
 import React, { useState } from 'react'
 import { graphql } from 'gatsby'
 import Popup from '../components/popup'
+import VideoPlayer from "../components/videoplayer"
 
 
 
-const VideosPage = ({ data }) => {
+const VideosPage = ( {data} ) => {
+  // console.log(props)
 const [buttonPopup, setButtonPopup] = useState(false);
+const [single, setSingle] = useState(null);
+
 
 return (
-        <div>
-        <p>VideosPage</p>
-        {data.allContentfulVideoPost.edges.map(vid => (
-            <div key={vid.node.video[0].id}>
-                <h3>{vid.node.title}</h3>
-                <button onClick={() => setButtonPopup(true)}><img src={vid.node.image.file.url} alt="Images"></img></button>
-                <Popup trigger={buttonPopup} setTrigger={setButtonPopup}></Popup>
-            </div>
-        ))}
-        </div> 
-)  
+  <div>
+    <p>VideosPage</p>
+    {data.allContentfulVideoPost.edges.map(vid => (
+      <div key={vid.node.video[0].id}>
+        <h3>{vid.node.title}</h3>
+        <button onClick={() => setButtonPopup(true)}><img src={vid.node.image.file.url} alt="Images"></img></button>
+      </div>
+    ))}
+    <Popup trigger={buttonPopup} setTrigger={setButtonPopup} url={data.allContentfulVideoPost.edges}/>
+  </div>
+)
+
+        
+        {/* {vid.node.video.map(x => (
+          <div> */}
+
+          {/* <VideoPlayer videoSrcURL={x.file.url} /> */}
+          {/* <Popup key={x.id} url={x.file.url} trigger={buttonPopup} triggerOne={single} setTrigger={setButtonPopup}></Popup>
+          </div>
+    ))} */}
+        
+      
+  
+  
+
+// return (<div>asdsd
+//         <p>VideosPage</p>
+//         {data.allContentfulVideoPost.edges.map(vid => (
+//             <div key={vid.node.video[0].id}>
+//                 <h3>{vid.node.title}</h3>
+//                 <button onClick={() => setButtonPopup(true)}><img src={vid.node.image.file.url} alt="Images"></img></button>
+//                 <Popup trigger={buttonPopup} setTrigger={setButtonPopup}></Popup>
+//             </div>
+//          ))} 
+//         </div> 
+// )  
 }
 
 
